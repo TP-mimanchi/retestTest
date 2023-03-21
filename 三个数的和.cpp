60518ac6b6a8dc3d227a8da4b1a7b9232f787fa9
TP-mimanchi <2540166403@qq.com>
2023-03-22 00:11:04 +0800

    F2双指针

--- 三个数的和.cpp
+++ 三个数的和.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int abs(int n)
@@ -15,19 +15,43 @@ int main()
     cin >> n >> target;
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    int minSum = 110011;
-    int Min = 0;
-    for (int i = 0; i < n - 2; i++)
-        for (int j = i + 1; j < n - 1; j++)
-            for (int k = j + 1; k < n; k++)
-            {
-                int sum = a[i] + a[j] + a[k];
-                if (abs(minSum) > abs(target - sum))
-                {
-                    minSum = target - sum;
-                    Min = sum;
-                }
-            }
-    cout << Min;
+    // int minSum = 110011;
+    // int Min = 0;
+    // for (int i = 0; i < n - 2; i++)
+    //     for (int j = i + 1; j < n - 1; j++)
+    //         for (int k = j + 1; k < n; k++)
+    //         {
+    //             int sum = a[i] + a[j] + a[k];
+    //             if (abs(minSum) > abs(target - sum))
+    //             {
+    //                 minSum = target - sum;
+    //                 Min = sum;
+    //             }
+    //         }
+    // cout << Min;
+
+    // 双指针
+    int minSum = a[0] + a[1] + a[2];
+    sort(a, a + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int start = i + 1, end = n - 1;
+        while (start < end)
+        {
+            int sum = a[i] + a[start] + a[end];
+            if (abs(target - minSum) > abs(target - sum))
+                minSum = sum;
+            if (sum > target)
+                --end;
+            else if (sum < target)
+                ++start;
+            else
+                break;
+        }
+        if (start < end)
+            break;
+    }
+    cout << minSum;
     return 0;
 }
