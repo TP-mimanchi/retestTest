0839a6af7634f29026799ea1527adfbcc1edd5d3
TP-mimanchi <2540166403@qq.com>
2023-03-23 13:20:03 +0800

    F3_回溯_函数substr限制

--- 有效ip.cpp
+++ 有效ip.cpp
@@ -16,25 +16,27 @@ void dfs(int n, string &st, int k)
 
     for (int i = 1; i <= 3; i++)
     {
-        string s = st.substr(k, i);
-        // cout << s << endl;
-        if (s[0] == '0')
+        if (k + i <= st.size()) // 函数substr的限制——pos不能超过st长度
         {
-            sip[n] = "0";
-            dfs(n + 1, st, k + i);
-            sip[n] = ""; // 回溯
-            break;
-        }
-        else
-        {
-            int x = 0;
-            for (int j = 0; j < i; j++)
-                x = x * 10 + (s[j] - '0');
-            if (x > 0 && x <= 255)
+            string s = st.substr(k, i);
+            if (s[0] == '0')
             {
-                sip[n] = s;
+                sip[n] = "0";
                 dfs(n + 1, st, k + i);
                 sip[n] = ""; // 回溯
+                break;
+            }
+            else
+            {
+                int x = 0;
+                for (int j = 0; j < i; j++)
+                    x = x * 10 + (s[j] - '0');
+                if (x > 0 && x <= 255)
+                {
+                    sip[n] = s;
+                    dfs(n + 1, st, k + i);
+                    sip[n] = ""; // 回溯
+                }
             }
         }
     }
